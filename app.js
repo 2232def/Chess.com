0687e4e962614ecd7ddcb5c8b867370250138c92
Dev Kumar Singh <gohi772004@gmail.com>
2025-05-26 15:29:34 +0530

    Added homepage

--- app.js
+++ app.js
@@ -23,7 +23,10 @@ let currentplayer = "w";
 app.set("view engine", "ejs");
 app.use(express.static(path.join(__dirname, "public")));
 
-app.get("/", function (req, res) {
+app.get("/",function(req, res){
+  res.render("homepage")
+})
+app.get("/play", function (req, res) {
   res.render("index");
 });
 
